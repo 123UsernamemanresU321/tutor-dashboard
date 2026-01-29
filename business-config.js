016a1ee07282a10895b1fc64c1be350a44d04ad6
123UsernamemanresU321 <erichuang.shangjing@outlook.com>
2026-01-29 20:01:28 +0200

    Remove meetingDomain from business-config.js

--- business-config.js
+++ business-config.js
@@ -4,6 +4,5 @@ window.BUSINESS_CONFIG = {
   merchantName: 'Qin Yu',
   supportEmail: 'eh8125809@gmail.com',
   businessAddress: 'Cape Town, South Africa',
-  termsEffectiveDate: '22 December 2025',
-  meetingDomain: 'vpaas-magic-cookie-3a002f9858684be6b26ca08b3c12b1e7.8x8.vc'
+  termsEffectiveDate: '22 December 2025'
 };
