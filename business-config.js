2d17ec8d8823d10320fc52f95f02552c8a7e6c17
123UsernamemanresU321 <erichuang.shangjing@outlook.com>
2026-02-17 22:51:54 +0200

    Update business-config.js

--- business-config.js
+++ business-config.js
@@ -2,7 +2,7 @@ window.BUSINESS_CONFIG = {
   brandName: 'Tutor Control Center',
   operatorName: 'Shangjing Huang',
   merchantName: 'Qin Yu',
-  supportEmail: 'eh8125809@gmail.com',
+  supportEmail: 'info@tutor-mcp.com',
   businessAddress: 'Cape Town, South Africa',
   termsEffectiveDate: '22 December 2025'
 };
